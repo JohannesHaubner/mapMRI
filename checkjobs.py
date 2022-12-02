ef6e6a36ad8e066a51ca7c8187e16deffd058ca6
Bastian <bazapf@math.uio.no>
2022-12-02 18:05:23 +0100

    check jobs and resubmit failed ones

---
+++ checkjobs.py
@@ -0,0 +1,36 @@
+import os
+import pathlib
+import time
+jobpath = "/home/bastian/Oscar-Image-Registration-via-Transport-Equation/slurm/"
+
+while True:
+    for job in sorted(os.listdir(jobpath)):
+
+        if not job.endswith(".out"):
+            
+            continue
+        jobfile = jobpath + job
+
+        jobid = str(pathlib.Path(job).stem)
+
+        file1 = open(jobfile, 'r')
+
+
+        try:
+            Lines = file1.readlines()
+        except UnicodeDecodeError:
+            print("UnicodeDecodeError at job", job, "will continue to next job")
+            continue
+
+        for line in Lines:
+            # print(line)
+
+            if "Killed" in line and "error" in line:
+                os.system("scancel " + jobid)
+                # print(line)
+                print("Cancelled job", jobid)
+                break
+
+
+    print("Sleeping for one hour")
+    time.sleep(60 * 60)
