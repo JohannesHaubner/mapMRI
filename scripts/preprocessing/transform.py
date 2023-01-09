6f525845119f14d8a217dbb04a26acd51738fd89
bzapf <bastivonzapf@gmail.com>
2023-01-09 16:37:02 +0100

    bug fixes in meshtransform

--- scripts/preprocessing/transform.py
+++ scripts/preprocessing/transform.py
@@ -8,37 +8,49 @@ import numpy as np
 from fenics import *
 from fenics_adjoint import *
 from nibabel.affines import apply_affine
-from dgregister import MRI2FEM, DGTransport, find_velocity_ocd, mesh_tools
-from mask_mri import get_bounding_box
 
-# from IPython import embed
+from dgregister.meshtransform import map_mesh, make_mapping
 
 
-from dgregister.meshtransform import map_meshes
 
-def path_to_wmparc(subj):
-    return "/home/basti/programming/Oscar-Image-Registration-via-Transport-Equation/mri2fem-dataset/freesurfer/" + subj + "/mri/wmparc.mgz"
+if "home/bastian" in os.getcwd():
+    resultpath = "/home/bastian/D1/registration/mriregistration_outputs/"
 
+else:
+    path_to_data = "/home/basti/programming/Oscar-Image-Registration-via-Transport-Equation/mri2fem-dataset/processed/coarsecropped/"
+    resultpath = "/home/basti/programming/Oscar-Image-Registration-via-Transport-Equation/registration/mriregistration_outputs/"
+    def path_to_wmparc(subj):
 
-path_to_data = "/home/basti/programming/Oscar-Image-Registration-via-Transport-Equation/mri2fem-dataset/processed/coarsecropped/"
-resultpath = "/home/basti/programming/Oscar-Image-Registration-via-Transport-Equation/registration/mriregistration_outputs/"
+        if subj == "abby":
+            return "/home/basti/programming/Oscar-Image-Registration-via-Transport-Equation/mri2fem-dataset/processed/registered/abbytoernie.mgz"
+        elif subj == "ernie":
 
-# jobfoldername = "OCDRK1A1e-05LBFGS1000"
-jobfoldername = "OCDRK1A0.001LBFGS1000"
+            return "/home/basti/programming/Oscar-Image-Registration-via-Transport-Equation/mri2fem-dataset/freesurfer/ernie/mri/wmparc.mgz"
+
+        else:
+            raise ValueError
+
+# # jobfoldername = "OCDRK1A1e-05LBFGS1000"
+# jobfoldername = "OCDRK1A0.001LBFGS1000"
+# jobfile = resultpath + jobfoldername + "/"
+# velocityfilename = "opts/opt_phi_204.h5"
+# readname = "function"
+
+jobfoldername = "E100A0.0001LBFGS100NOSMOOTHEN"
 jobfile = resultpath + jobfoldername + "/"
-velocityfilename = "opts/opt_phi_204.h5"
-res = 16
-
-if __name__ == "__main__":
-    pass
+velocityfilename = "VelocityField.hdf"
+readname = "-1"
 
 hyperparameters = json.load(open(jobfile + "hyperparameters.json"))
 
-FName = pathlib.Path(path_to_data) / pathlib.Path(hyperparameters["input"]).name
-FName = str(FName)
+
 
 if "OCD" not in jobfile:
-    raise NotImplementedError
+    ocd = False
+    assert hyperparameters["max_timesteps"] > 1
+else:
+    ocd = True
+    assert hyperparameters["max_timesteps"] == 1
 
 res = 16
 
@@ -46,12 +58,9 @@ subj1 = "abby"
 subj2 = "ernie"
 
 path1 = "chp4/outs/" + subj1 + "/"
+path2 = "chp4/outs/" + subj2 + "/"
 
-
-
-
-state_functionspace = "DG"
-
+os.makedirs(jobfile + "postprocessing/", exist_ok=True)
 
 mapfile = jobfile + "postprocessing/" + "all" + ".hdf"
 mappingname = "coordinatemapping"
@@ -61,6 +70,8 @@ ny = hyperparameters["input.shape"][1]
 nz = hyperparameters["input.shape"][2]
 
 cubemesh = BoxMesh(MPI.comm_world, Point(0.0, 0.0, 0.0), Point(nx, ny, nz), nx, ny, nz)
+
+print("cubemesh size", nx, ny, nz)
 
 V3 = VectorFunctionSpace(cubemesh, hyperparameters["velocity_functionspace"], hyperparameters["velocity_functiondegree"],)
 
@@ -73,113 +84,64 @@ if os.path.isfile(mapfile):
     print("Read mapping")
 
 else:
+    # FName = pathlib.Path(path_to_data) / pathlib.Path(hyperparameters["input"]).name
+    # FName = str(FName)
 
     # cubemesh, cubeimg, _ = MRI2FEM.read_image(hyperparameters={"image": FName, "state_functionspace": state_functionspace, "state_functiondegree":1}, 
     #                 name="image", mesh=None, normalize=False, threshold=False)
 
 
     # V = VectorFunctionSpace(cubemesh, hyperparameters["velocity_functionspace"], hyperparameters["velocity_functiondegree"],)
-
-
-    V1 = FunctionSpace(cubemesh, "CG", 1)
-
-    os.makedirs(jobfile + "postprocessing/", exist_ok=True)
-    unity2 = Function(V1) #¤ cubeimg.function_space())
-    unity2.vector()[:] = 0
-
-    mappings = []
-
-    for coordinate in ["x[0]", "x[1]", "x[2]"]:
-
-        if os.path.isfile(jobfile + "postprocessing/" + coordinate + ".hdf"):
-
-            coordinate_mapping = Function(V1)
-
-            hdf = HDF5File(cubemesh.mpi_comm(), jobfile + "postprocessing/" + coordinate + ".hdf", "r")
-            hdf.read(coordinate_mapping, "out")
-            hdf.close()
-
-            assert norm(coordinate_mapping) != 0
-
-            mappings.append(coordinate_mapping)
-
-        else:
-
-            xin = interpolate(Expression(coordinate, degree=1), V1) # cubeimg.function_space())
-
-            v = Function(V3)
-            vfile = jobfile + velocityfilename
-            hdf = HDF5File(cubemesh.mpi_comm(), vfile, "r")
-            hdf.read(v, "function")
-
-            hdf.close()
-
-            print("Inverting velocity for backtransport")
-            v.vector()[:] *= (-1)
-
-            assert norm(v) > 0
-
-            print("Transporting, ", coordinate, "coordinate")
-
-            xout, phi_c0 = find_velocity_ocd.find_velocity(Img=xin, Img_goal=unity2, hyperparameters=hyperparameters, phi_eval=v, projection=False)
-
-            assert norm(xout) != 0
-
-            mappings.append(xout)
-
-            assert os.path.isdir(jobfile + "postprocessing/")
-            # xout = xin
-
-            hdf = HDF5File(cubemesh.mpi_comm(), jobfile + "postprocessing/" + coordinate + ".hdf", "w")
-            # hdf.write(cubemesh, "mesh")
-            hdf.write(xin, "in")
-            hdf.write(xout, "out")
-            hdf.close()
-
-            with XDMFFile(jobfile + "postprocessing/" + coordinate + "_in.xdmf") as xdmf:
-                xdmf.write_checkpoint(xin, "xin", 0.)
-
-
-            with XDMFFile(jobfile + "postprocessing/" + coordinate + "_out.xdmf") as xdmf:
-                xdmf.write_checkpoint(xout, "xout", 0.)
-
-    assert len(mappings) == 3
-
-    assert True not in [norm(x) == 0 for x in mappings]
-
-    # for coordinate in ["x[0]", "x[1]", "x[2]"]:
-    vxyz = Function(V3)
-
-    arr = np.zeros_like(vxyz.vector()[:])
-
-    arr[0::3] = mappings[0].vector()[:]
-    arr[1::3] = mappings[1].vector()[:]
-    arr[2::3] = mappings[2].vector()[:]
-
-    assert np.sum(arr) != 0
-
-    vxyz.vector()[:] = arr
+    v = Function(V3)
+    vfile = jobfile + velocityfilename
+    hdf = HDF5File(cubemesh.mpi_comm(), vfile, "r")
+    hdf.read(v, readname)
+    hdf.close()
     
-    mapping = vxyz
+    
+    mapping = make_mapping(cubemesh, v, jobfile, hyperparameters, ocd=ocd)
 
-    assert norm(vxyz) != 0
 
     hdf = HDF5File(cubemesh.mpi_comm(), mapfile, "w")
-    hdf.write(vxyz, mappingname)
+    hdf.write(mapping, mappingname)
     hdf.close()
     print("Created vector function that maps xyz mesh coordinates")
 
+if "home/bastian" in os.getcwd():
+    print("Done, exiting")
+    exit()
 
-xmlfile2 = path1 + subj2 + str(res) + ".xml"
+xmlfile2 = path2 + subj2 + str(res) + ".xml"
 xmlfile1 = path1 + subj1 + str(res) + ".xml"
 
 imgfile1 = path_to_wmparc(subj1)
-imgfile2 = path_to_wmparc(subj2) 
+imgfile2 = path_to_wmparc(subj2)
+
+assert "abby" in imgfile1
+assert "ernie" in imgfile2
+
 box = np.load(path_to_data + "box.npy")
+
+
+inputmesh = Mesh(xmlfile2)
+print("targetmesh.coordinates().min = ", np.min(inputmesh.coordinates(), axis=0))
+print("targetmesh.coordinates().max = ", np.max(inputmesh.coordinates(), axis=0))
+
+targetmesh = Mesh(xmlfile2)
+print("targetmesh.coordinates().min = ", np.min(targetmesh.coordinates(), axis=0))
+print("targetmesh.coordinates().max = ", np.max(targetmesh.coordinates(), axis=0))
+# exit()
+
+
+brainmesh2 = map_mesh(xmlfile1, imgfile1, imgfile2, mapping, box=box, 
+                    outfolder=jobfile + "postprocessing/",
+                    coarsening_factor=2)
 
 xmlfile3 = jobfile + "postprocessing/" + "transformed_input_mesh.xdmf"
 with XDMFFile(xmlfile3) as xdmf:
     xdmf.write(brainmesh2) # , "mesh")
+
+brainmesh = Mesh(xmlfile1)
 
 for m, mname in zip([brainmesh, brainmesh2],["original", "transformed"]):
     print(mname, MeshQuality.radius_ratio_min_max(m))
--- src/dgregister/meshtransform.py
+++ src/dgregister/meshtransform.py
@@ -8,23 +8,49 @@ import numpy as np
 from fenics import *
 from fenics_adjoint import *
 from nibabel.affines import apply_affine
-from dgregister import MRI2FEM, DGTransport, find_velocity_ocd, mesh_tools
-from mask_mri import get_bounding_box
+from dgregister.helpers import get_largest_box, pad_with, cut_to_box, get_bounding_box, get_lumped_mass_matrices
+from dgregister import find_velocity_ocd
+
+
+
+def store2mgz(imgfile1, imgfile2, ijk1, outfolder):
+    assert os.path.isdir(outfolder)
+
+    data = np.zeros((256, 256, 256))
+
+    for idx, img in enumerate([imgfile1, imgfile2]):
+        image1 = nibabel.load(img)
+        
+         
+        i1, j1, k1 = numpy.rint(ijk1).astype("int")
+
+        data[i1, j1, k1] = 1.
+
+        assert data.sum() > 100
+
+        nii = nibabel.Nifti1Image(data, image1.affine)
+        # breakpoint()
+        nibabel.save(nii, outfolder + pathlib.Path(imgfile1).name.replace(".mgz", "affine" + str(idx) + "_meshindices.mgz"))
 
 # from IPython import embed
 
-def map_meshes(xmlfile1, imgfile1, imgfile2, box=None):
+def map_mesh(xmlfile1, imgfile1, imgfile2, mapping, box=None, coarsening_factor=2, outfolder=None):
 
+    comm = MPI.comm_world
+    nprocs = comm.Get_size()
 
-    args = {"mesh": xmlfile1}
+    if nprocs != 1:
+        raise NotImplementedError
 
-    if args["mesh"].endswith(".xml"):
-        brainmesh = Mesh(args["mesh"])
+    if int(coarsening_factor) not in [1, 2]:
+        raise ValueError
+
+    if xmlfile1.endswith(".xml"):
+        brainmesh = Mesh(xmlfile1)
     else:
         brainmesh = Mesh()
-        hdf = HDF5File(brainmesh.mpi_comm(), args["mesh"], "r")
+        hdf = HDF5File(brainmesh.mpi_comm(), xmlfile1, "r")
         hdf.read(brainmesh, "/mesh", False)
-
 
     image1 = nibabel.load(imgfile1)
 
@@ -34,23 +60,53 @@ def map_meshes(xmlfile1, imgfile1, imgfile2, box=None):
     xyz1 = brainmesh.coordinates()
 
     ijk1 = apply_affine(ras2vox1, xyz1).T
-    i1, j1, k1 = numpy.rint(ijk1).astype("int")
+    # i1, j1, k1 = numpy.rint(ijk1).astype("int")
+
+    store2mgz(imgfile1, imgfile2, ijk1, outfolder)
+
+    i1, j1, k1 = ijk1
+
+    print("Maximum values of indices before transform", i1.max(), j1.max(), k1.max())
 
     if box is not None:
     
-    bounds = get_bounding_box(box)
+        bounds = get_bounding_box(box)
 
-    dxyz = [bounds[x].start for x in range(3)]
-    # TODO FIXME is this correct ? 
+        dxyz = [bounds[x].start for x in range(3)]
+        # TODO FIXME is this correct ? 
+    else:
+
+        dxyz = [0, 0, 0]
+
+    npad = 4 #  + 1
 
     ijk2 = []
     for i,j, k in zip(i1, j1, k1):
-        i = (i - dxyz[0]) / 2
-        j = (j - dxyz[1]) / 2
-        k = (k - dxyz[2]) / 2
+        i = (i + npad - dxyz[0]) / coarsening_factor
+        j = (j + npad - dxyz[1]) / coarsening_factor
+        k = (k + npad - dxyz[2]) / coarsening_factor
         
-        i2, j2, k2 = mapping((i,j,k))
+        point = (i,j,k)
+        try:
+            i2, j2, k2 = mapping(point)
+        except RuntimeError:
+            print(point, "not in BoxMesh")
+            exit()
+
+        if max([i2, j2, k2]) > 256:
+            point = (i,j,k)
+            print(point, "-->", (i2, j2, k2))
+            exit()
+        
         ijk2.append([i2, j2, k2])
+
+
+    print("Maximum values of indices after transform", np.max(ijk2, axis=0))
+
+    if np.max(ijk2) > 255:
+        raise ValueError
+
+
 
     image2 = nibabel.load(imgfile2)
 
@@ -58,19 +114,133 @@ def map_meshes(xmlfile1, imgfile1, imgfile2, box=None):
     vox2ras2 = image2.header.get_vox2ras_tkr()
     # ras2vox_tkr_inv2 = numpy.linalg.inv(vox2ras2)
 
+
+
+
+
     ijk2 = np.array(ijk2)
 
     print(ijk2.dtype, ijk2.shape)
 
     xyz2 = apply_affine(vox2ras2, ijk2)
 
-    if args["mesh"].endswith(".xml"):
-        brainmesh2 = Mesh(args["mesh"])
+    if xmlfile1.endswith(".xml"):
+        brainmesh2 = Mesh(xmlfile1)
     else:
         brainmesh2 = Mesh()
-        hdf = HDF5File(brainmesh2.mpi_comm(), args["mesh"], "r")
+        hdf = HDF5File(brainmesh2.mpi_comm(), xmlfile1, "r")
         hdf.read(brainmesh2, "/mesh", False)
 
-    # embed()
 
     brainmesh2.coordinates()[:] = xyz2
+
+    return brainmesh2
+
+
+
+def make_mapping(cubemesh, v, jobfile, hyperparameters, ocd):
+
+
+    mappings = []
+
+    for coordinate in ["x[0]", "x[1]", "x[2]"]:
+
+        # if os.path.isfile(jobfile + "postprocessing/" + coordinate + ".hdf"):
+
+        #     coordinate_mapping = Function(V1)
+
+        #     hdf = HDF5File(cubemesh.mpi_comm(), jobfile + "postprocessing/" + coordinate + ".hdf", "r")
+        #     hdf.read(coordinate_mapping, "out")
+        #     hdf.close()
+
+        #     assert norm(coordinate_mapping) != 0
+
+        #     mappings.append(coordinate_mapping)
+
+        # else:
+
+        
+
+        # print("Inverting velocity for backtransport")
+        # v.vector()[:] *= (-1)
+
+        assert norm(v) > 0
+
+        print("Transporting, ", coordinate, "coordinate")
+
+        if ocd:
+            V1 = FunctionSpace(cubemesh, "CG", 1)
+
+            
+            unity2 = Function(V1) #¤ cubeimg.function_space())
+            unity2.vector()[:] = 0
+
+
+            xin = interpolate(Expression(coordinate, degree=1), V1) # cubeimg.function_space())
+            xout, _ = find_velocity_ocd.find_velocity(Img=xin, Img_goal=unity2, hyperparameters=hyperparameters, phi_eval=-v, projection=False)
+        else:
+            import dgregister.config as config
+            
+            config.hyperparameters = hyperparameters
+            from dgregister.DGTransport import Transport
+
+
+            V1 = FunctionSpace(cubemesh, hyperparameters["state_functionspace"], hyperparameters["state_functiondegree"])
+            xin = interpolate(Expression(coordinate, degree=1), V1) # cubeimg.function_space())
+            
+            if hyperparameters["smoothen"]:
+                raise NotImplementedError
+                _, M_lumped_inv = get_lumped_mass_matrices(vCG=vCG)
+            else:
+                M_lumped_inv = None
+            print("Calling Transport()")
+
+            xout = Transport(Img=xin, Wind=-v, hyperparameters=hyperparameters,
+                            MaxIter=hyperparameters["max_timesteps"], DeltaT=hyperparameters["DeltaT"], timestepping=hyperparameters["timestepping"], 
+                            solver=hyperparameters["solver"], MassConservation=hyperparameters["MassConservation"])
+            # find_velocity.find_velocity(Img=xin, Img_goal=unity2, hyperparameters=hyperparameters, phi_eval=-v, files=[], projection=False)
+            # find_velocity(Img=Img, Img_goal=Img_goal, vCG=vCG, M_lumped_inv=M_lumped_inv, hyperparameters=hyperparameters, files=files, starting_guess=controlfun)
+            V1_CG = FunctionSpace(cubemesh, "CG", 1)
+
+            xout = project(xout, V1_CG)
+            print("Projected xout to CG1")
+
+
+        assert norm(xout) != 0
+
+        mappings.append(xout)
+
+        assert os.path.isdir(jobfile + "postprocessing/")
+
+        hdf = HDF5File(cubemesh.mpi_comm(), jobfile + "postprocessing/" + coordinate + ".hdf", "w")
+        hdf.write(xin, "in")
+        hdf.write(xout, "out")
+        hdf.close()
+
+        with XDMFFile(jobfile + "postprocessing/" + coordinate + "_in.xdmf") as xdmf:
+            xdmf.write_checkpoint(xin, "xin", 0.)
+
+
+        with XDMFFile(jobfile + "postprocessing/" + coordinate + "_out.xdmf") as xdmf:
+            xdmf.write_checkpoint(xout, "xout", 0.)
+
+    assert len(mappings) == 3
+
+    assert True not in [norm(x) == 0 for x in mappings]
+
+    # for coordinate in ["x[0]", "x[1]", "x[2]"]:
+    vxyz = Function(v.function_space())
+
+    arr = np.zeros_like(vxyz.vector()[:])
+
+    arr[0::3] = mappings[0].vector()[:]
+    arr[1::3] = mappings[1].vector()[:]
+    arr[2::3] = mappings[2].vector()[:]
+
+    assert np.sum(arr) != 0
+
+    vxyz.vector()[:] = arr
+    
+    assert norm(vxyz) != 0
+
+    return vxyz
