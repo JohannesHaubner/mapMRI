513f150de53f66f3faec9d25801865427a7e769d
Bastian <bazapf@math.uio.no>
2023-02-03 10:51:36 +0100

    reset preconditioning to a3d757d9176c306a20f42d680af431cd1cd906cd

--- src/dgregister/preconditioning.py
+++ src/dgregister/preconditioning.py
@@ -1,28 +1,15 @@
 from dolfin import *
-
-# import dgregister.config as config
-# def print_overloaded(*args):
-#     if MPI.rank(MPI.comm_world) == 0:
-#         # set_log_level(PROGRESS)
-#         print(*args)
-#     else:
-#         pass
-# # if ocd:
-
-# if "optimize" in config.hyperparameters.keys() and (not config.hyperparameters["optimize"]):
-#     print_overloaded("Not importing dolfin-adjoint")
-# else:
-#     print_overloaded("Importing dolfin-adjoint")
-#     from dolfin_adjoint import *
-
 from dolfin_adjoint import *
+
+import numpy as np
+
 from dgregister.config import hyperparameters
 assert len(hyperparameters) > 1
 
 def print_overloaded(*args):
     if MPI.rank(MPI.comm_world) == 0:
         # set_log_level(PROGRESS)
-        print(*args, flush=True)
+        print(*args)
     else:
         pass
 
@@ -57,13 +44,12 @@ def print_overloaded(*args):
 
 
 
+
 class Preconditioning():
 
     def __init__(self) -> None:
         # self.smoothen = hyperparameters["smoothen"]
         # self.hyperparameters = hyperparameters
-
-        # print("Class is initialized, MPI rank=" + str(MPI.rank(MPI.comm_world)), flush=True)
 
         pass
 
@@ -84,38 +70,27 @@ class Preconditioning():
 
             # print_overloaded("Debugging: cc ", cc.vector()[:].min(), cc.vector()[:].max(), cc.vector()[:].mean())
 
-            return cc
-
         else:
             C = func.function_space()
             dim = func.geometric_dimension()
-
-            if not hasattr(self, "BC"):
-
-                self.BC = DirichletBC(C, Constant((0.0,)*dim), "on_boundary")
-                self.c = TrialFunction(C)
-                self.psi = TestFunction(C)
-                self.cc = Function(C)
-                # self.matrix
+            BC = DirichletBC(C, Constant((0.0,)*dim), "on_boundary")
+            c = TrialFunction(C)
+            psi = TestFunction(C)
+            
 
             if not hasattr(self, "solver"):
-                a = inner(grad(self.c), grad(self.psi)) * dx
+                a = inner(grad(c), grad(psi)) * dx
+                # a = inner(grad(c), grad(psi)) * dx
                 self.A = assemble(a)
-
-                print_overloaded("Assembled A in Preconditioning() with ", func)
-
-                self.BC.apply(self.A)
-                print_overloaded("Applying BC (at init, i.e. only once?) to func=", func)
+                print_overloaded("Assembled A in Preconditioning()")
             
-            L = inner(func, self.psi) * dx
+            L = inner(func, psi) * dx
             
             
             tmp = assemble(L)
-            self.BC.apply(tmp)
+            BC.apply(tmp)
             
-            # self.BC.apply(self.A)
-            # print_overloaded("Applying BC! to func=", func)
-
+            BC.apply(self.A)
             # solve(a == L, c, BC)
 
             if not hasattr(self, "solver"):
@@ -132,78 +107,17 @@ class Preconditioning():
                     self.solver = PETScKrylovSolver("gmres", hyperparameters["preconditioner"])
                     self.solver.set_operators(self.A, self.A)
 
-                    print_overloaded("Created Krylov solver (For the first time ? ) in Preconditioning() with ", func)
+                    print_overloaded("Created Krylov solver in Preconditioning()")
 
 
             # BC.apply(self.A)
             # x = args[0]
             # b = args[1]
 
-            print_overloaded("Solving in preconditioning, func=", func)
-            self.solver.solve(self.cc.vector(), tmp)
+            cc = Function(C)
+            self.solver.solve(cc.vector(), tmp)
 
-            return self.cc
-
+        return cc
 
 
 preconditioning = Preconditioning()
-
-
-# def preconditioning(func):
-
-#     if not hyperparameters["smoothen"]:
-
-#         # print_overloaded("applying BC to func in Preconditioning()")
-#         cc = func.copy()
-
-#         # breakpoint()
-
-#         # print_overloaded("Debugging: cc ", cc.vector()[:].min(), cc.vector()[:].max(), cc.vector()[:].mean())
-#         C = cc.function_space()
-#         dim = cc.geometric_dimension()
-#         BC=DirichletBC(C, Constant((0.0,)*dim), "on_boundary")
-#         BC.apply(cc.vector())
-#     else:
-#         C = func.function_space()
-#         dim = func.geometric_dimension()
-
-
-
-#         BC = DirichletBC(C, Constant((0.0,)*dim), "on_boundary")
-#         c = TrialFunction(C)
-#         psi = TestFunction(C)
-#         cc = Function(C)
-#         # self.matrix
-
-#         a = inner(grad(c), grad(psi)) * dx
-#         # a = inner(grad(c), grad(psi)) * dx
-#         A = assemble(a)
-#         print_overloaded("Assembled A in Preconditioning() with", func)
-        
-#         L = inner(func, psi) * dx
-        
-        
-#         tmp = assemble(L)
-#         BC.apply(tmp)
-        
-#         BC.apply(A)
-#         # solve(a == L, c, BC)
-
-
-#         solver = PETScKrylovSolver("gmres", hyperparameters["preconditioner"])
-#         solver.set_operators(A, A)
-
-#         print_overloaded("Created Krylov solver in Preconditioning() with ", func)
-
-
-#         # BC.apply(self.A)
-#         # x = args[0]
-#         # b = args[1]
-
-        
-#         solver.solve(cc.vector(), tmp)
-
-#     return cc
-
-
-# preconditioning = lambda x: x
--- src/dgregister/preconditioning_overloaded.py
+++ src/dgregister/preconditioning_overloaded.py
@@ -1,32 +1,15 @@
 from dolfin import *
-
-# import dgregister.config as config
-# def print_overloaded(*args):
-#     if MPI.rank(MPI.comm_world) == 0:
-#         # set_log_level(PROGRESS)
-#         print(*args)
-#     else:
-#         pass
-# # if ocd:
-
-# if "optimize" in config.hyperparameters.keys() and (not config.hyperparameters["optimize"]):
-#     print_overloaded("Not importing dolfin-adjoint")
-# else:
-#     print_overloaded("Importing dolfin-adjoint")
-#     from dolfin_adjoint import *
-
 from dolfin_adjoint import *
-
-def print_overloaded(*args):
-    if MPI.rank(MPI.comm_world) == 0:
-        # set_log_level(PROGRESS)
-        print(*args, flush=True)
-    else:
-        pass
 
 from pyadjoint import Block
 from pyadjoint.overloaded_function import overload_function
 
+def print_overloaded(*args):
+    if MPI.rank(MPI.comm_world) == 0:
+        # set_log_level(PROGRESS)
+        print(*args)
+    else:
+        pass
 
 import numpy as np
 
@@ -45,52 +28,13 @@ backend_preconditioning = preconditioning
 
 
 class PreconditioningBlock(Block):
-    
     def __init__(self, func, **kwargs):
         super(PreconditioningBlock, self).__init__()
         self.kwargs = kwargs
         self.add_dependency(func)
         # self.smoothen = smoothen
 
-        C = func.function_space()
-        dim = func.geometric_dimension()
-        
-        self.C = C
-        self.dim = dim
-            # if not hasattr(self, "BC"):
-
         # self.smoothen = hyperparameters["smoothen"]
-        self.BC = DirichletBC(C, Constant((0.0,) * dim), "on_boundary")
-        self.c = TrialFunction(C)
-        self.psi = TestFunction(C)
-        self.ct = Function(C)
-        self.ctest = TestFunction(C)
-        self.matrix = assemble(inner(self.c, self.psi) * dx)
-     
-        #     if not hasattr(self, "solver"):
-        a = inner(grad(self.c), grad(self.psi)) * dx
-        self.A = assemble(a)
-        self.BC.apply(self.A)
-
-        print_overloaded("Assembled A in PreconditioningBlock() with ", func)
-        
-        # if True:
-        if hyperparameters["solver"] == "lu":
-
-            self.solver = LUSolver()
-            self.solver.set_operator(self.A)
-            print_overloaded("Created LU solver in PreconditioningBlock()")
-
-        elif hyperparameters["solver"] == "krylov":
-            self.solver = PETScKrylovSolver("gmres", hyperparameters["preconditioner"])
-            # 
-            # print_overloaded("type of A", type(self.A), self.A)
-            # print_overloaded("type of self.solver", type(self.solver))
-            print_overloaded("Created Krylov solver in PreconditioningBlock() with ", func)
-
-            self.solver.set_operators(self.A, self.A)
-    
-
 
     def __str__(self):
         return 'PreconditioningBlock'
@@ -107,29 +51,47 @@ class PreconditioningBlock(Block):
             tmp = adj_inputs[0].copy()
             BC.apply(tmp)
         else:
-            # tmp = adj_inputs[0]
-            
-            # tmp = adj_inputs[0]
-            # print_overloaded("NOT copying")
-            # print_overloaded("*"*80)
-
-
             tmp = adj_inputs[0].copy()
-            print_overloaded("Copying")
-            print_overloaded("*"*80)
+            C = inputs[idx].function_space()
+            dim = inputs[idx].geometric_dimension()
+            BC = DirichletBC(C, Constant((0.0,) * dim), "on_boundary")
+            c = TrialFunction(C)
+            psi = TestFunction(C)
+            
+            if not hasattr(self, "solver"):
+                a = inner(grad(c), grad(psi)) * dx
+                self.A = assemble(a)
+                BC.apply(self.A)
+
+                print_overloaded("Assembled A in PreconditioningBlock()")
+                
+                # if True:
+                if hyperparameters["solver"] == "lu":
+
+                    self.solver = LUSolver()
+                    self.solver.set_operator(self.A)
+                    print_overloaded("Created LU solver in PreconditioningBlock()")
+
+                elif hyperparameters["solver"] == "krylov":
+                    self.solver = PETScKrylovSolver("gmres", hyperparameters["preconditioner"])
+                    # 
+                    # print_overloaded("type of A", type(self.A), self.A)
+                    # print_overloaded("type of self.solver", type(self.solver))
+                    print_overloaded("Created Krylov solver in PreconditioningBlock()")
+
+                    self.solver.set_operators(self.A, self.A)
+            
             
             # a = inner(grad(c), grad(psi)) * dx
             # A = assemble(a)
+            ct = Function(C)
             
-            
-            self.BC.apply(tmp)
-            
-            print_overloaded("Solving in preconditioning_overloaded, tmp=", tmp)
-            self.solver.solve(self.ct.vector(), tmp)
+            BC.apply(tmp)
+            self.solver.solve(ct.vector(), tmp)
             # solve(self.A, ct.vector(), tmp)
-            
-            # tmp = assemble(inner(self.ctest, self.ct) * dx)
-            tmp = self.matrix * self.ct.vector()
+            ctest = TestFunction(C)
+            tmp = assemble(inner(ctest, ct) * dx)
+        
         
         return tmp
 
@@ -137,5 +99,3 @@ class PreconditioningBlock(Block):
         return backend_preconditioning(inputs[0])
 
 preconditioning = overload_function(preconditioning, PreconditioningBlock)
-
-#preconditioning = lambda x: x
